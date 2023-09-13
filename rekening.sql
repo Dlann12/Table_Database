ff84baa0bd1dd98b63f7c3245d9c331fb721334f
Fadlan dwi Febrio <130965620+Dlann12@users.noreply.github.com>
2023-09-14 00:26:12 +0700

    rekening.sql

--- rekening.sql
+++ rekening.sql
@@ -1,7 +1,7 @@
 CREATE TABLE rekening (
     No_rekening INT(30) PRIMARY KEY,
     saldo INT(10),
-    Jenis_rekening VARCHAR(20),
-    status VARCHAR(20),
+    Jenis_rekening ENUM('Tabungan','Giro','Deposito','dll'),
+    status ENUM('AKTIF','TIDAK AKTIF'),
     Tanggal_pembuatan DATE
 );

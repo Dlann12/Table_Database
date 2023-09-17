3afc6662798756f1eb960de6c5bf5aa5fea7dca8
Fadlan dwi Febrio <130965620+Dlann12@users.noreply.github.com>
2023-09-17 16:29:11 +0700

    teller.sql

--- teller.sql
+++ teller.sql
@@ -1,4 +1,9 @@
-CREATE TABLE teller (
-    Id_teller INT(30) PRIMARY KEY,
-    Nama_teller VARCHAR(20)
+CREATE TABLE TELLER (
+    Id_teller INTEGER(30) PRIMARY KEY,
+    Nama_teller VARCHAR(20),
+    Alamat VARCHAR(30),
+    Email VARCHAR(20),
+    No_telepon INT(15),
+    Jenis_kelamin ENUM('laki_laki', 'Perempuan'),
+    Tanggal_lahir DATE
 );

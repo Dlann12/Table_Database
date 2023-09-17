47496ba1e785cc963247b5357b71f55cddb1bd68
Fadlan dwi Febrio <130965620+Dlann12@users.noreply.github.com>
2023-09-17 16:28:44 +0700

    direktur.sql

--- direktur.sql
+++ direktur.sql
@@ -1,4 +1,11 @@
-CREATE TABLE direktur_bank (
-    Id_direktur INT(30) PRIMARY KEY,
-    Nama_direktur VARCHAR(20)
+CREATE TABLE DIREKTUR_BANK (
+    Id_direktur INTEGER(30) PRIMARY KEY,
+    Nama_direktur VARCHAR(20),
+    Id_cabang INT(20),
+    Nomor_telepon INTEGER(15),
+    Email VARCHAR(20),
+    Tanggal_bergabung DATE,
+    Alamat VARCHAR(30),
+    Jenis_kelamin ENUM('Laki-Laki', 'Perempuan'),
+    FOREIGN KEY (Id_cabang) REFERENCES CABANG_BANK(Id_cabang)
 );

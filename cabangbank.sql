13a4e4210a4446e33b7de6ca5545fb44a45e2d7b
Fadlan dwi Febrio <130965620+Dlann12@users.noreply.github.com>
2023-09-17 16:28:14 +0700

    cabangbank.sql

--- cabangbank.sql
+++ cabangbank.sql
@@ -1,5 +1,10 @@
-CREATE TABLE cabang_bank (
-    Id_cabang INT(20) PRIMARY KEY,
+CREATE TABLE CABANG_BANK (
+    Id_cabang INTEGER PRIMARY KEY,
     Alamat_cabang VARCHAR(30),
-    Nama_cabang VARCHAR(20)
+    Id_direktur INT,
+    Nama_cabang VARCHAR(20),
+    Nomor_telepon INTEGER,
+    Email_cabang VARCHAR(20),
+    Kedudukan_cabang ENUM('pusat', 'anak'),
+    FOREIGN KEY (Id_direktur) REFERENCES TABEL_DIREKTUR(Id_direktur)
 );
